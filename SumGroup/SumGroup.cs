597073325082d3416100895767dd41217e7e2180
weida <dadamouse@gmail.com>
2017-01-11 02:48:34 +0800

    1. 來源可以是「任何型別的集合」 2. 可以任意決定幾筆一組 3. 回傳每一組 Σf(x) 的集合 Σ結果型別可直接用int

---
+++ SumGroup/SumGroup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumGroup
+{
+    public class SumGroup
+    {
+
+
+        private static void Main(string[] args)
+        {
+            //var rawData = new List<T>
+            //{
+            //    new Data<T> { Id=1, Cost=1, Revenue=11, SellPrice=21},
+            //    new Data<T> { Id=2, Cost=2, Revenue=12, SellPrice=22},
+            //};
+
+            //var type = rawData.GetType();
+            //var propertyInfos = type.GetProperties();
+
+
+            //foreach (var raw in rawData) {
+            //    var detail = raw.GetType().GetProperties().FirstOrDefault(x => x.Name == "Cost").GetValue(raw);
+            //}
+            //string command = Console.ReadLine();
+            // Console.Write("Enter Command: ");
+        }
+
+        
+        public List<int> getGroupSum<T>(List<T> rawData, string field, int group)
+        {
+            if (rawData == null)
+            {
+                throw new ArgumentException("rawData cannot be null");
+            }
+
+            if (field == null)
+            {
+                throw new ArgumentException("field cannot be null");
+            }
+
+            if (group < 1)
+            {
+                throw new ArgumentException("group is not correct");
+            }
+
+            var sumList = new List<int>();
+            int sum = 0;
+            // start by 1
+            int i = 1;
+
+            //check for has next run
+            bool flag = false;
+            foreach (var raw in rawData)
+            {
+                flag = false;
+
+                try
+                {
+                    sum += (int)raw.GetType().GetProperties().FirstOrDefault(x => x.Name == field).GetValue(raw);
+                }
+                catch (NullReferenceException e)
+                {
+                    //null exception
+                    throw new Exception(e.ToString());
+                }
+
+                //insert to list
+                if (i % group == 0)
+                {
+                    sumList.Add(sum);
+                    Console.WriteLine("{0}", sum);
+                    sum = 0;
+                    flag = true;
+                }
+                i++;
+            }
+
+            //check last run is happened
+            if (!flag) sumList.Add(sum);
+
+            //var sumList = new List<int> { 6, 15, 24, 21 };
+            return sumList;
+        }
+    }
+
+    //Any type of list
+    public class Data
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }
+    }
+}
---
+++ SumGroupTests/SumGroupTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using SumGroup;
+
+namespace SumGroup.Tests
+{
+    [TestClass()]
+    //1. 來源可以是「任何型別的集合」
+    //2. 可以任意決定幾筆一組
+    //3. 回傳每一組 Σf(x) 的集合
+    //- Σ結果型別可直接用int
+    public class SumGroupTests
+    {
+        [TestMethod()]
+        public void SumTest_field_cost_group_3_get_6_15_24_21()
+        {
+            //arrange
+            var rawData = new List<Data>
+            {
+                new Data { Id=1, Cost=1, Revenue=11, SellPrice=21},
+                new Data { Id=2, Cost=2, Revenue=12, SellPrice=22},
+                new Data { Id=3, Cost=3, Revenue=13, SellPrice=23},
+                new Data { Id=4, Cost=4, Revenue=14, SellPrice=24},
+                new Data { Id=5, Cost=5, Revenue=15, SellPrice=25},
+                new Data { Id=6, Cost=6, Revenue=16, SellPrice=26},
+                new Data { Id=7, Cost=7, Revenue=17, SellPrice=27},
+                new Data { Id=8, Cost=8, Revenue=18, SellPrice=28},
+                new Data { Id=9, Cost=9, Revenue=19, SellPrice=29},
+                new Data { Id=10, Cost=10, Revenue=20, SellPrice=30},
+                new Data { Id=11, Cost=11, Revenue=21, SellPrice=31},
+            };
+
+            var field = "Cost";
+            int group = 3;
+            var expected = new List<int> { 6, 15, 24, 21 };
+            SumGroup sumGroup = new SumGroup();
+
+            //act
+            var actual = sumGroup.getGroupSum(rawData, field, group);
+
+            //assert
+            actual.ShouldBeEquivalentTo(expected);  
+        }
+
+        [TestMethod()]
+        public void SumTest_field_Revenue_group_4_get_50_66_60()
+        {
+            //arrange
+            var rawData = new List<Data>
+            {
+                new Data { Id=1, Cost=1, Revenue=11, SellPrice=21},
+                new Data { Id=2, Cost=2, Revenue=12, SellPrice=22},
+                new Data { Id=3, Cost=3, Revenue=13, SellPrice=23},
+                new Data { Id=4, Cost=4, Revenue=14, SellPrice=24},
+                new Data { Id=5, Cost=5, Revenue=15, SellPrice=25},
+                new Data { Id=6, Cost=6, Revenue=16, SellPrice=26},
+                new Data { Id=7, Cost=7, Revenue=17, SellPrice=27},
+                new Data { Id=8, Cost=8, Revenue=18, SellPrice=28},
+                new Data { Id=9, Cost=9, Revenue=19, SellPrice=29},
+                new Data { Id=10, Cost=10, Revenue=20, SellPrice=30},
+                new Data { Id=11, Cost=11, Revenue=21, SellPrice=31},
+            };
+
+            var field = "Revenue";
+            int group = 4;
+            var expected = new List<int> { 50, 66, 60 };
+            SumGroup sumGroup = new SumGroup();
+
+            //act
+            var actual = sumGroup.getGroupSum(rawData, field, group);
+
+            //assert
+            actual.ShouldBeEquivalentTo(expected);
+        }
+    }
+}
